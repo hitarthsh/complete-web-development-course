bd93c3f63932606adf98d6e5205614555c498730
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2024-12-16 20:21:16 +0530

    add_change

---
+++ JSFOUNDATION/part1/changes.js
@@ -0,0 +1,20 @@
+let gameName = "spiderman";
+
+gameName = "batman";
+
+console.log(gameName);
+
+// const can not change value
+const username = "hitarthshah";
+
+username = "hitarth";
+
+console.log(username);
+
+/*
+--Keywords--
+
+var
+let
+const
+*/
