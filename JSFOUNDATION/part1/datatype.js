1808b7039f2409a2e8960b48a7cbdfff8cb2d27d
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2024-12-16 20:10:11 +0530

    add_data_types

---
+++ JSFOUNDATION/part1/datatype.js
@@ -0,0 +1,36 @@
+console.log("hello");
+// console.log("chai");
+/*
+
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+Object
+
+Symbol
+
+*/
+
+// var score = 102
+
+// Number
+let score = 102;
+
+// String
+let name = "chaicode.com";
+
+// Boolean
+let isLoggedin = true;
+
+//Object
+let teaTypes = ["lemon tea", "orange tea", "oolong tea"];
+let user = { firstname: "Hitarth", lastname: "Shah" };
+
+let getScore = score;
+
+console.log(getScore);
