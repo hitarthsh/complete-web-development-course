9081d6eabb8adeaf1d1230fb115b35965b3b1740
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2024-12-18 23:41:47 +0530

    add_non_primitive

---
+++ JSFOUNDATION/part2/nonPrimitive.js
@@ -0,0 +1,34 @@
+// this is Object
+let username = {
+  "first name": "hitarth",
+  isLoggedin: true,
+};
+
+username.firstname = "Mr. H";
+username.lastname = "shah";
+
+// console.log(username["first name"]);
+// console.log(username.lastname);
+// console.log(username);
+// console.log(typeof username);
+
+let today = new Date();
+// console.log(today.getDate());
+
+// Array
+
+let anotherUser = ["hitarth", true];
+
+// console.log(anotherUser[0]);
+
+console.log(1 + "1"); // 11
+console.log("1" + 1); // 11
+
+console.log(1 + 1); // 2
+
+// let isValue = true;
+// console.log( (isValue)); //2
+
+let isValue = "2abc";
+console.log(typeof Number(isValue));
+console.log(Number(null));
