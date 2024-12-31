91c5b5e6b37db408482c6cdd9c77bef3f71b88d1
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2024-12-31 21:42:34 +0530

    add_1

--- JSFOUNDATION/part5/functions.js
+++ JSFOUNDATION/part5/functions.js
@@ -3,6 +3,12 @@
 Store the result in a variable named `teaOrder`.
 */
 
+function makeTea(typeOfTea) {
+  return `Making ${typeOfTea}`;
+}
+let teaOrder = makeTea("green tea");
+console.log(teaOrder);
+
 /* 
 2. Create a function named `orderTea` that takes one parameter, `teaType`. Inside this function, create another function named `confirmOrder` that returns a message like `"Order confirmed for chai"`. 
 Call `confirmOrder` from within `orderTea` and return the result.
