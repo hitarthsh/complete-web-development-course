79d9604ad505a9b60de4a335019fd64fc8b1b96c
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2024-12-31 22:00:12 +0530

    add_3

--- JSFOUNDATION/part5/functions.js
+++ JSFOUNDATION/part5/functions.js
@@ -7,12 +7,22 @@ function makeTea(typeOfTea) {
   return `Making ${typeOfTea}`;
 }
 let teaOrder = makeTea("green tea");
-console.log(teaOrder);
+// console.log(teaOrder);
 
 /* 
 2. Create a function named `orderTea` that takes one parameter, `teaType`. Inside this function, create another function named `confirmOrder` that returns a message like `"Order confirmed for chai"`. 
 Call `confirmOrder` from within `orderTea` and return the result.
 */
+
+function orderTea(teaType) {
+  function confirmOrder() {
+    return `Order confirmed for chai`;
+  }
+  return confirmOrder();
+}
+
+let orderConfirmation = orderTea("chai");
+console.log(orderConfirmation);
 
 /* 
 3. Write an arrow function named `calculateTotal` that takes two parameters: `price` and `quantity`. The function should return the total cost by multiplying the `price` and `quantity`. 
